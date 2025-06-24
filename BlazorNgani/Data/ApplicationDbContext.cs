43548c42621cb5008d27db3d0c58d3c235669bed
Clinttttt <clintvillanueva82@gmail.com>
2025-06-25 00:05:29 +0800

    Add project files.

---
+++ BlazorNgani/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BlazorNgani.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorNgani.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<ProductDetails> ProductInfo { get; set; }
+        protected ApplicationDbContext()
+        {
+        }
+    }
+}
---
+++ BlazorNgani/Models/ProductDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorNgani.Models
+{
+    public class ProductDetails
+    {
+        public int Id { get; set; }
+        public string? Product { get; set; }
+        public string? Genre { get; set; }
+        public double Price { get; set; }
+        public string? BookDescription { get; set; }
+    }
+}
