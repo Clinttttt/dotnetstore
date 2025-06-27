3371565f21f75995aaab28be8541404596935d7e
Clinttttt <clintvillanueva82@gmail.com>
2025-06-27 23:18:31 +0800

    changes

--- BlazorNgani/Program.cs
+++ BlazorNgani/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -17,11 +17,11 @@ Options.AddPolicy("AdminOnly", policy =>
 policy.RequireRole("Admin")));
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
     app.UseHsts();
 }
 
